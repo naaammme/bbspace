04f0dab6099528279579ba796048bacf3a4fd89f
hualahuala <16347434+hualahuala1@user.noreply.gitee.com>
2026-03-29 22:08:22 +0800

    build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.kotlin.dsl.configure
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -12,6 +16,22 @@ plugins {
 }
 
 subprojects {
+    pluginManager.withPlugin("com.android.application") {
+        extensions.configure<ApplicationExtension> {
+            lint {
+                disable += "NullSafeMutableLiveData"
+            }
+        }
+    }
+
+    pluginManager.withPlugin("com.android.library") {
+        extensions.configure<LibraryExtension> {
+            lint {
+                disable += "NullSafeMutableLiveData"
+            }
+        }
+    }
+
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
